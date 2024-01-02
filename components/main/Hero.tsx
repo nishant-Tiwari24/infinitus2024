1556c818cce138d61f76be43c72292a0aabb709a
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-01-03 00:37:53 +0530

    media queries

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -7,13 +7,10 @@ import Image from 'next/image';
 const Hero: React.FC = () => {
   return (
     <section className="relative w-full flex flex-col bg-contain first-section">
-      <div className="left-[20%] right-[20%] flex-wrap absolute max-w-screen h-[500px] object-contain z-20 top-[-600px]">
-        <Image width={1005} priority height={198} src="/img.png" alt="" />
+      <div className="left-[20%] right-[20%] flex-wrap absolute max-w-screen h-[500px] object-contain z-20 top-[-700px]">
+        <Image width={1005} priority height={198} src="/images/inifinitus24-min.png" alt="" />
       </div>
       <Timer />
-      {/* <div className="text-center left-[40%] mt-4 absolute top-[-180px] text-yellow-100">
-        <p className="text-sm font-bold h3">22nd February 2024</p>
-      </div> */}
     </section>
   );
 };
