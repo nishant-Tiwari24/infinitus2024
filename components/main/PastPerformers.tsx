4d8760ff5da48d121c85f344335cedf447c61133
Omkar-Ghongade <omkarsubhashghongade21@gmail.com>
2024-01-03 15:11:13 +0530

    Image slider for Past Performers

--- components/main/PastPerformers.tsx
+++ components/main/PastPerformers.tsx
@@ -11,87 +11,58 @@ const MyServices: React.FC<MyServicesProps> = () => {
     <section className="section service bg-fixed bg-center bg-cover" style={{ backgroundImage: `url(${backgroundImageUrl})` }} id="service" aria-labelledby="service-lable">
       <p className="section-subtitle container" id="service-lable">Past Performers</p>
 
-      <ul className="service-list">
-        <li data-reveal>
-          <div className="service-card container">
+      <div className="slider">
+          <div className="slider-track">
+            <div className="slide">
             <Image src="/images/service-1.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
-
-            <div>
-              <h3 className="h5 text-white card-title">Armaan Malik</h3>
-              <p className="card-subtitle">Aut temporibus qui vero.</p>
             </div>
-
-            <Link href="#" passHref>
-              <div className="btn-icon" aria-label="See more">
-                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="20" viewBox="0 0 43 20" fill="none">
-                  <path d="M0 10H41" stroke="white" strokeWidth="2" />
-                  <path d="M33 1L41.9 10.2727L33 19" stroke="white" strokeWidth="2" />
-                </svg>
-              </div>
-            </Link>
-          </div>
-        </li>
-
-        <li data-reveal>
-          <div className="service-card container">
-            <Image src="/images/service-2.jpg" width={340} height={380} loading="lazy" alt="Event Organiser" className="img" />
-
-            <div>
-              <h3 className="h5 text-white card-title">Lost Stories</h3>
-              <p className="card-subtitle">Nobis, magni modi ipsa culpa.</p>
+            <div className="slide">
+            <Image src="/images/service-2.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
             </div>
-
-            <Link href="#" passHref>
-              <div className="btn-icon" aria-label="See more">
-                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="20" viewBox="0 0 43 20" fill="none">
-                  <path d="M0 10H41" stroke="white" strokeWidth="2" />
-                  <path d="M33 1L41.9 10.2727L33 19" stroke="white" strokeWidth="2" />
-                </svg>
-              </div>
-            </Link>
-          </div>
-        </li>
-
-        <li data-reveal>
-          <div className="service-card container">
-            <Image src="/images/service-3.jpg" width={340} height={380} loading="lazy" alt="Product Marketing" className="img" />
-
-            <div>
-              <h3 className="h5 text-white card-title">Aakshar</h3>
-              <p className="card-subtitle">Tenetur porro repellendus!</p>
+            <div className="slide">
+            <Image src="/images/service-3.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
             </div>
-
-            <Link href="#" passHref>
-              <div className="btn-icon" aria-label="See more">
-                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="20" viewBox="0 0 43 20" fill="none">
-                  <path d="M0 10H41" stroke="white" strokeWidth="2" />
-                  <path d="M33 1L41.9 10.2727L33 19" stroke="white" strokeWidth="2" />
-                </svg>
-              </div>
-            </Link>
-          </div>
-        </li>
-
-        <li data-reveal>
-          <div className="service-card container">
-            <Image src="/images/service-4.jpg" width={340} height={380} loading="lazy" alt="Videography" className="img" />
-
-            <div>
-              <h3 className="h5 text-white card-title">Project 91</h3>
-              <p className="card-subtitle h7">Amet consectetur adipisicing elit</p>
+            <div className="slide">
+            <Image src="/images/service-4.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
             </div>
-
-            <Link href="#" passHref>
-              <div className="btn-icon" aria-label="See more">
-                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="20" viewBox="0 0 43 20" fill="none">
-                  <path d="M0 10H41" stroke="white" strokeWidth="2" />
-                  <path d="M33 1L41.9 10.2727L33 19" stroke="white" strokeWidth="2" />
-                </svg>
-              </div>
-            </Link>
+            <div className="slide">
+            <Image src="/images/service-1.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-2.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-3.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-4.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-1.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-2.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-3.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-4.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-1.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-2.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-3.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
+            <div className="slide">
+            <Image src="/images/service-4.jpg" width={340} height={380} loading="lazy" alt="Wedding Photography" className="img" />
+            </div>
           </div>
-        </li>
-      </ul>
+      </div>
     </section>
   );
 };
