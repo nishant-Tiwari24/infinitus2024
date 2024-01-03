a18fc6101ce1bce5431cf4bdba727dfa73cd8cec
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-01-03 13:16:10 +0530

    Update Proshows.tsx

--- components/main/Proshows.tsx
+++ components/main/Proshows.tsx
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react'
 import Image from 'next/image';
+import { url } from 'inspector';
+
 
 const Proshows: React.FC = () => {
-  const [imageLoading, setImageLoading] = useState<boolean>(true);
+    const urlbackground = '/images/img-backgroun.jpg';
 
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      setImageLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(delay);
-  }, []);
-
-  const urlbackground = '/images/img-backgroun.jpg';
-  
   return (
     <main>
-      <section
-        className="section prowshows bg-cover bg-fixed z-0"
-        id="proshows"
-        style={{ backgroundImage: `url(${urlbackground})` }}
-        aria-labelledby="proshows-label"
-      >
+      <section className="section prowshows bg-cover bg-fixed z-0" id="proshows" style={{backgroundImage:`url(${urlbackground})`}} aria-labelledby="proshows-label">
         <div className="container">
           <div className="proshows-list">
             <div className="wrapper">
@@ -29,19 +16,9 @@ const Proshows: React.FC = () => {
                 Prowshows
               </h2>
 
-              {/* Proshow 1 */}
               <div className="proshows-card" data-reveal>
-                <figure className="card-banner img-holder has-before" style={{ width: "700px", height: "605px" }}>
-                  {imageLoading && <div className="image-loader">Loading...</div>}
-                  <Image
-                    src="/images/portfolio-1.jpg"
-                    width={700}
-                    height={605}
-                    loading="lazy"
-                    alt="Shoe Promo"
-                    className={`img-cover ${imageLoading ? 'hidden' : ''}`}
-                    onLoad={() => setImageLoading(false)}
-                  />
+                <figure className="card-banner img-holder has-before" style={{ width:"700px", height:"605px"}}>
+                  <Image src="/images/portfolio-1.jpg" width={700} height={605} loading="lazy" alt="Shoe Promo" className="img-cover" />
                 </figure>
 
                 <div className="card-content">
@@ -62,19 +39,9 @@ const Proshows: React.FC = () => {
                 </a>
               </div>
 
-              {/* Proshow 2 */}
               <div className="proshows-card" data-reveal>
-                <figure className="card-banner img-holder has-before" style={{ width: "700px", height: "1091px" }}>
-                  {imageLoading && <div className="image-loader">Loading...</div>}
-                  <Image
-                    src="/images/portfolio-2.jpg"
-                    width={700}
-                    height={1091}
-                    loading="lazy"
-                    alt="Wedding Shot"
-                    className={`img-cover ${imageLoading ? 'hidden' : ''}`}
-                    onLoad={() => setImageLoading(false)}
-                  />
+                <figure className="card-banner img-holder has-before" style={{  width:"700px", height:"1091px"}}>
+                  <Image src="/images/portfolio-2.jpg" width={700} height={1091} loading="lazy" alt="Wedding Shot" className="img-cover" />
                 </figure>
 
                 <div className="card-content">
@@ -95,19 +62,13 @@ const Proshows: React.FC = () => {
                 </a>
               </div>
 
-              {/* Proshow 3 */}
+              </div>
+
+              <div className="wrapper">
+
               <div className="proshows-card" data-reveal>
-                <figure className="card-banner img-holder has-before" style={{ width: "700px", height: "1000px" }}>
-                  {imageLoading && <div className="image-loader">Loading...</div>}
-                  <Image
-                    src="/images/portfolio-3.jpg"
-                    width={700}
-                    height={1000}
-                    loading="lazy"
-                    alt="Fashion Show"
-                    className={`img-cover ${imageLoading ? 'hidden' : ''}`}
-                    onLoad={() => setImageLoading(false)}
-                  />
+                <figure className="card-banner img-holder has-before" style={{ width:"700px", height:"1000px"}}>
+                  <Image src="/images/portfolio-3.jpg" width={700} height={1000} loading="lazy" alt="Fashion Show" className="img-cover" />
                 </figure>
 
                 <div className="card-content">
@@ -128,19 +89,9 @@ const Proshows: React.FC = () => {
                 </a>
               </div>
 
-              {/* Proshow 4 */}
               <div className="proshows-card" data-reveal>
-                <figure className="card-banner img-holder has-before" style={{ width: "700px", height: "850px" }}>
-                  {imageLoading && <div className="image-loader">Loading...</div>}
-                  <Image
-                    src="/images/portfolio-4.jpg"
-                    width={700}
-                    height={850}
-                    loading="lazy"
-                    alt="Jumbo Barger"
-                    className={`img-cover ${imageLoading ? 'hidden' : ''}`}
-                    onLoad={() => setImageLoading(false)}
-                  />
+                <figure className="card-banner img-holder has-before" style={{  width:"700px", height:"850" }}>
+                  <Image src="/images/portfolio-4.jpg" width={700} height={850} loading="lazy" alt="Jumbo Barger" className="img-cover" />
                 </figure>
 
                 <div className="card-content">
@@ -160,8 +111,12 @@ const Proshows: React.FC = () => {
                   </svg>
                 </a>
               </div>
+
             </div>
           </div>
+{/* 
+          <Image src="/images/portfolio-shape.svg" width={286} height={232} loading="lazy" alt="" className="shape" /> */}
+
         </div>
       </section>
     </main>
