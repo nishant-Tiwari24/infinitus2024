2c05c00c5ab208b3e37ffccdc9828fa70eba21e0
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-01-05 13:29:56 +0530

    Update Timer.tsx

--- components/sub/Timer.tsx
+++ components/sub/Timer.tsx
@@ -43,7 +43,7 @@ const Timer: React.FC = () => {
     <section className="timer-container ">
       <section className="timer absolute left-1/2 -translate-x-1/2 -translate-y-1/2 top-[-300px]">
         <div className="timer-class ">
-        <div className="flex flex-row font-light font-italic  justify-center gap-1 text-3xl sm:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-white to-[#AAAAAA]">
+        <div className="flex  font-light font-italic  gap-1 text-3xl sm:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-white to-[#AAAAAA]">
         <div className="flex flex-col gap-2 text-center">
           <span>{timeLeft.days}</span>
           <span className="text-sm lg:text-lg">Days</span>
