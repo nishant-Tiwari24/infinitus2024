7dbbb0a68002f2eb1f275cfadc1118b5cc79db3e
Omkar Subhash Ghongade <88375748+Omkar-Ghongade@users.noreply.github.com>
2024-01-10 01:52:28 +0530

    Update Hero.tsx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
-    <section className="relative w-full h-screen flex items-center justify-center bg-contain first-section">
-      <div className="max-w-screen object-contain z-20">
+    <section className="relative w-full flex flex-col bg-contain first-section">
+      <div className="flex-wrap absolute max-w-screen object-contain z-20 top-[-600px] lg:left-[350px] lg:top-[-700px] lg:right-[-100px]">
         <Image width={1005} priority height={198} src="/images/inifinitus24-min.png" alt="" />
       </div>
       <Timer />
