5801906827ad1948cba2832206ef171e8b0bcf2c
Omkar-Ghongade <omkarsubhashghongade21@gmail.com>
2024-01-27 00:02:52 +0530

    about page

--- components/main/About.tsx
+++ components/main/About.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="container mx-auto my-10">
+    <div className="container items-center mx-auto my-10">
       <h1 className="text-5xl lg:text-5xl font-bold mb-8 text-center">About Us</h1>
-      <div className="flex flex-col lg:flex-row items-center lg:justify-between p-8">
-        <img
-          src="/img (1).jpg"
-          alt="About Us"
-          style={{ width: '450px', height: '500px' }}
-          className="w-full lg:w-1/2 h-auto lg:h-full mb-8 object-cover lg:mb-0 lg:mr-8 rounded-3xl"
-        />
-        <div className="lg:w-1/2 flex flex-col gap-10">
+      <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between p-8 gap-16">
+        <div className="mx-auto">
+          <img
+            src="/img (1).jpg"
+            alt="About Us"
+            style={{ width: '450px', height: '500px' }}
+            className="w-full h-auto mb-8 object-cover rounded-3xl"
+          />
+        </div>
+        <div className="lg:w-1/2 pr-20">
           <h2 className="text-3xl lg:text-4xl font-semibold mb-4 lg:mb-6 text-left lg:text-left ">
             Embark on the Extravaganza: SRM University&apos;s Annual Cultural Fest Unveiled!
           </h2>
-          <p className="text-4xl lg:text-3xl text-justify">
+          <p className="text-2xl lg:text-3xl text-justify">
             Welcome to the grand celebration of SRM University&apos;s annual cultural fest! Get
             ready for an extraordinary experience filled with vibrant performances, creative
             expressions, and unforgettable moments. We&apos;re thrilled to have you join us for
@@ -28,7 +30,7 @@ const About = () => {
             distinctio.
           </p>
         </div>
-   </div>
+      </div>
     </div>
   );
 };
