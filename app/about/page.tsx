1f6572723b82b1b486f71fa96598f6a4c20c3a4f
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-05 16:48:09 +0530

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import StarsCanvas from '@/components/main/StarCanvas'
 import Navbar from '@/components/main/Navbar'
 import Footer from '@/components/main/Footer'
+import AboutPage from '@/components/main/AboutPage'
 
 function page() {
   return (
     <div className="flex flex-col h-[950px] gap-20 bg-cover bg-fixed">
         <Navbar/>
       <StarsCanvas />
+      <AboutPage/>
     </div>
   )
 }
