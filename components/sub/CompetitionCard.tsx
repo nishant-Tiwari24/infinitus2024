49d7206292f1d1572570269a569cfc7635238825
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-24 18:04:09 +0530

    loader css

--- components/sub/CompetitionCard.tsx
+++ components/sub/CompetitionCard.tsx
@@ -16,7 +16,7 @@ const Card: React.FC<CardProps> = ({ imageSrc, title, initialDescription, linkTo
   useEffect(() => {
     const loaderTimeout = setTimeout(() => {
       setImageLoading(false);
-    }, 2000); 
+    }, 2000);
     return () => {
       clearTimeout(loaderTimeout);
     };
@@ -25,18 +25,19 @@ const Card: React.FC<CardProps> = ({ imageSrc, title, initialDescription, linkTo
   return (
     <div className='px-[10px] lg:p-0'>
       <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-        {imageLoading && (
-          <div className="h-[600px] w-[400px] flex justify-center items-center">
-            <BeatLoader color="#ffffff" loading={imageLoading} size={15} />
-          </div>
-        )}
-        <div className="h-[600px] w-[400px] flex justify-center items-center">
-        <img
-          className={`h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125 ${imageLoading ? 'hidden' : ''}`}
-          src={imageSrc}
-          alt=""
-          onLoad={() => setImageLoading(false)}
-        />
+        <div className="h-[600px] w-[400px] flex justify-center items-center relative">
+          {imageLoading && (
+            <div className="absolute inset-0 flex justify-center items-center">
+              <BeatLoader color="#ffffff" loading={imageLoading} size={15} />
+            </div>
+          )}
+          <img
+            className={`h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125 ${imageLoading ? 'hidden' : ''}`}
+            src={imageSrc}
+            alt=""
+            onLoad={() => setImageLoading(false)}
+          />
+        </div>
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/80 group-hover:to-black/90"></div>
         <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <p className="mb-10 text-3xl text-white opacity-0 transition-opacity duration-800 group-hover:opacity-100">{initialDescription}</p>
@@ -47,7 +48,6 @@ const Card: React.FC<CardProps> = ({ imageSrc, title, initialDescription, linkTo
               See Rules
             </button>
           </Link>
-        </div>
         </div>
       </div>
     </div>
