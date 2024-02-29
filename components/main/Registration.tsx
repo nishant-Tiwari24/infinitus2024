2b262069a71381de53f4322e19ee8d35c725ffa8
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-29 17:21:13 +0530

    Update Registration.tsx

--- components/main/Registration.tsx
+++ components/main/Registration.tsx
@@ -7,7 +7,7 @@ function Registration() {
     <div className='w-full flex items-center justify-center h-screen'>
       <div className="text-center">
         <div className="font-bold font-space text-4xl mb-4">Registration Coming Soon!</div>
-        <div className="text-lg text-gray-200 font-space">We're working on bringing you a seamless registration experience. Stay tuned!</div>
+        <div className="text-lg text-gray-200 font-space">We are working on bringing you a seamless registration experience. Stay tuned!</div>
       </div>
     </div>
     <Footer/>
