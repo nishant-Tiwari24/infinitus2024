59a7dd55d9a5a68a76573addf9649a48f03527e5
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-03-11 03:55:06 +0530

    Update Card.tsx

--- components/sub/Card.tsx
+++ components/sub/Card.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export function ThreeDCardDemo() {
   return (
     <CardContainer className="inter-var flex gap-20 flex-col sm:flex-row">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-[40rem] sm:w-[60rem]  sm:h-[60rem] h-auto rounded-xl p-6 border  ">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-[40rem] sm:w-[60rem] px-6  sm:h-[60rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
           className="text-3xl sm:text-4xl font-space font-bold text-neutral-600 dark:text-white"
@@ -56,7 +56,7 @@ export function ThreeDCardDemo() {
       </CardBody>
       <div>
         <Link href={"https://unstop.com/p/music-fiesta-infinitus-2024-srm-university-srmap-andhra-pradesh-927977"}>
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-[40rem] sm:w-[60rem]  sm:h-[60rem] h-auto rounded-xl p-6 border  ">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-[40rem] sm:w-[60rem] px-6 sm:h-[60rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
           className="text-3xl sm:text-4xl font-space font-bold text-neutral-600 dark:text-white"
