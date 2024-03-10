0bc28ca1c507bcfb5b73ec80ff5850e1414bb121
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-03-11 04:09:27 +0530

    Update Registration.tsx

--- components/main/Registration.tsx
+++ components/main/Registration.tsx
@@ -9,7 +9,7 @@ function Registration() {
   return (
     <div className='pt-20'>
       <FlareCursor />
-      <h1 className='text-center text-7xl font-semibold Welcome-text text-transparent bg-gradient-to-r from-fuchsia-200 to-cyan-200 bg-clip-text font-mono py-20'>Registration</h1>
+      <h1 className='text-center text-5xl sm:text-7xl font-space font-bold Welcome-text text-transparent bg-gradient-to-r from-fuchsia-200 to-cyan-200 bg-clip-text font-mono py-20'>Registration</h1>
       <div className='container w-80 mx-auto flex justify-center items-center flex-wrap'>
        <ThreeDCardDemo/>
       </div>
